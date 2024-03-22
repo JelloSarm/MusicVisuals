5065ac64464ddc73e04ec9a3c5d592fb0f944c26
Jellytots04 <jello352@gmail.com>
2024-03-22 16:37:08 +0000

    Fixed the screen size in my audio visual function

--- java/src/C22531133/JellosVisual.java
+++ java/src/C22531133/JellosVisual.java
@@ -10,27 +10,26 @@ public class JellosVisual extends Visual{
     {
         this.js = js;
     }
-
-    int rows = (int) (200+height / 20);
-    int cols = (int) (width / 20);
+    int scale = 10;
+    int rows;
+    int cols;
     float[][] land;
 
     public void draw() {
-        rows = (int) (200+height / 20);
-        cols = (int) (width / 20);
+        rows = (int) (js.height / scale);
+        cols = (int) (js.width / scale);
 
         land = new float[cols][rows];
         js.background(0);
         js.stroke(255);
         js.noFill();
-
         for(int y = 0; y < rows-1;y++)
         {
             js.beginShape(TRIANGLE_STRIP);
             for(int x = 0; x < cols; x++)
             {
-                js.vertex(x * 20, y * 20, land[x][y]);
-                js.vertex(x * 20, (y+1)*20, land[x][y+1]);
+                js.vertex(x * scale, y * scale, land[x][y]);
+                js.vertex(x * scale, (y+1)*scale, land[x][y+1]);
             }
             js.endShape();
         }
