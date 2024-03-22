8a11d64542cf159565f046ab50422d55921b2c4b
Jellytots04 <jello352@gmail.com>
2024-03-22 16:50:32 +0000

    Pushed up some walls for the mountain terrain

--- java/src/C22531133/JellosVisual.java
+++ java/src/C22531133/JellosVisual.java
@@ -10,26 +10,37 @@ public class JellosVisual extends Visual{
     {
         this.js = js;
     }
-    int scale = 10;
+    int scale = 20;
     int rows;
     int cols;
     float[][] land;
 
     public void draw() {
-        rows = (int) (js.height / scale);
-        cols = (int) (js.width / scale);
+        int w = js.width;
+        int h = js.height;
+        rows = (int) (200 + h / scale);
+        cols = (int) (w / scale);
 
         land = new float[cols][rows];
         js.background(0);
         js.stroke(255);
         js.noFill();
+
+        js.translate(w / 2 , h / 2);
+        js.rotateX(PI/3);
+        js.translate(-w / 2, -h);
         for(int y = 0; y < rows-1;y++)
         {
             js.beginShape(TRIANGLE_STRIP);
             for(int x = 0; x < cols; x++)
             {
-                js.vertex(x * scale, y * scale, land[x][y]);
-                js.vertex(x * scale, (y+1)*scale, land[x][y+1]);
+                if(x == 0)
+                {
+                    js.vertex(x * 20, y * 20, land[x][y] + random(90, 130));
+                    js.vertex(x * 20, (y+1) * 20, land[x][y + 1] + random(90,130));
+                }
+                js.vertex(x * scale, y * scale, 1);
+                js.vertex(x * scale, (y+1)*scale, 1);
             }
             js.endShape();
         }
