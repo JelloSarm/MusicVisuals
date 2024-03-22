e5ac5272793d1f1eda85ff668503eaba218366dd
Jellytots04 <jello352@gmail.com>
2024-03-22 16:09:24 +0000

    Adding in bits for terrain

--- java/src/C22531133/JellosVisual.java
+++ java/src/C22531133/JellosVisual.java
@@ -3,5 +3,14 @@ package C22531133;
 import ie.tudublin.*;
 
 public class JellosVisual extends Visual{
-    //hello
+    int rows = (int) (200+height / 20);
+    int cols = (int) (width / 20);
+    float[][] land;
+
+    public void draw() {
+        rows = (int) (200+height / 20);
+        cols = (int) (width / 20);
+
+        land = new float[cols][rows];
+    }
 }
