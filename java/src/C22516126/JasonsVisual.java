53eeb7e9c6ed5ff39c6f7ba18cb12a7822f971b6
c22516126 <124163643+c22516126@users.noreply.github.com>
2024-03-24 20:54:31 +0000

    placement adjustments + optimized code

--- java/src/C22516126/JasonsVisual.java
+++ java/src/C22516126/JasonsVisual.java
@@ -6,8 +6,9 @@ import example.MyVisual;
 public class JasonsVisual extends Visual {
 
     MyVisual jg;
-    float moonsize = 300;
     float reversecount = 0;
+    float moonimplodesize = 300;
+    
 
     //variable to ensure scaling through different screen sizes
     public JasonsVisual(MyVisual jg)
@@ -16,8 +17,22 @@ public class JasonsVisual extends Visual {
     }
 
     public void render()
-    {
+    { 
+        //moon placement + size
+        //jg.width / 2, (jg.height * (float)0.65)
+        float moonX = jg.width*(float)0.4;
+        float moonY = jg.height * (float)0.65;
+        float moonsize = jg.width * (float)0.17;
+
+        //planet placement
+        //(-jg.height, (jg.height /2), -jg.width);
+        float planetX = jg.height*4;
+        float planetY = -jg.height * 2;
+        float planetZ = -jg.width;
+        float planetsize = jg.width* 2;
+
         float multiplier = jg.height * (float)0.2;
+        float planetmultiplier = jg.height * (float)0.6;
 
         float fov = jg.PI / (float)3.0; // A field of view of 60 degrees
         float cameraZ = (jg.height / 2.0f) / jg.tan(fov / 2.0f);
@@ -28,24 +43,51 @@ public class JasonsVisual extends Visual {
         0, 0, 1, 0);
 
         
+        //text settings
         jg.textAlign(CENTER, CENTER);
         jg.textSize(48); 
 
+        //detonation timer
         float countdown = (10000 - millis());
         String timer = String.format("%.2f", countdown / 1000);
 
+        //jg.circle(width, moonY, 40);
+         //stars
+         //float stars[][];
+         /*
+         for(int i = 0; i < 100; i++)
+         {
+             float row = 0;
+             float col = 0;
+ 
+             row = ((jg.width / 100)*i);
+             for(int j = 0; j < 40; j++)
+             {
+ 
+                 float offsetx = 0;
+                 float offsety = 0;
+ 
+                 offsetx = random(-10, 10);
+                 offsety = random(-10, 10);
+                 
+                 col = ((jg.height / 40)*j);
+                 jg.circle(row, col, 5);
+ 
+             }
+         }*/
+
         //planet
         jg.stroke(150, 255, 255);
-        //jg.fill(150, 255, 100);
-        jg.noFill();
+        jg.fill(150, 255, 50);
+        //jg.noFill();
         jg.pushMatrix();
-        jg.translate(-jg.height, (jg.height /2), -jg.width);
+        jg.translate(planetX, planetY, planetZ);
 
         //rotate the planet slowly
         //jg.rotateY(millis() / (float)(4000));
         jg.rotateX(millis() / (float)(80000));
 
-        jg.sphere(jg.width);
+        jg.sphere(planetsize);
         jg.popMatrix();
 
         //planet bounce
@@ -53,13 +95,13 @@ public class JasonsVisual extends Visual {
         {
             jg.noFill();
 
-            //looping colours
+            //loop a range of colours
             int hue = 150 + (i%105);
             jg.stroke(hue, 255, 255);
             
             jg.pushMatrix();
-            jg.translate(-jg.height, (jg.height /2), -jg.width);
-            jg.circle(0, 0, (jg.getAudioBuffer().get(i) * multiplier) + (jg.width * 2));
+            jg.translate(planetX, planetY, planetZ);
+            jg.circle(0, 0, (jg.getAudioBuffer().get(i) * planetmultiplier) + (planetsize * 2));
             jg.popMatrix();
 
 
@@ -75,31 +117,7 @@ public class JasonsVisual extends Visual {
             jg.stroke(200, 0, 255);
             jg.fill(200, 255, 255);
             jg.pushMatrix();
-            jg.translate(jg.width / 2, (jg.height * (float)0.65), 0);
-
-            //rotate the moon
-            jg.rotateY(millis() / (float)(4000));
-            jg.rotateX(millis() / (float)(80000));
-
-            jg.sphere(jg.width * (float)0.13);
-            jg.popMatrix();
-
-            reversecount = millis() + 3000;
-        }
-
-        //after moon detonates
-        if (countdown < 1 && moonsize > 0)
-        {
-            float unscaledMoonsize = reversecount - millis();
-            moonsize = unscaledMoonsize / 10;
-            jg.fill(0, 0, 255);
-            jg.text("Moon Detonation Immenent\n0.00", jg.width / 2, jg.height * (float)0.2);
-
-            //moon
-            jg.stroke(0, 255, 0);
-            jg.fill(0, 255, 255);
-            jg.pushMatrix();
-            jg.translate(jg.width / 2, (jg.height * (float)0.65), 0);
+            jg.translate(moonX, moonY, 0);
 
             //rotate the moon
             jg.rotateY(millis() / (float)(4000));
@@ -108,9 +126,33 @@ public class JasonsVisual extends Visual {
             jg.sphere(moonsize);
             jg.popMatrix();
 
+            reversecount = millis() + 3000;
+        }
+
+        //implosion begins
+        if (countdown < 1 && moonimplodesize > 0)
+        {
+            float unscaledMoonsize = reversecount - millis();
+            moonimplodesize = unscaledMoonsize / 10;
+            jg.fill(0, 200, 255);
+            jg.text("Moon Detonation Immenent\n0.00", jg.width / 2, jg.height * (float)0.2);
+
+            //moon
+            jg.stroke(0, 255, 0);
+            jg.fill(0, 255, 255);
+            jg.pushMatrix();
+            jg.translate(moonX, moonY, 0);
+
+            //rotate the moon
+            jg.rotateY(millis() / (float)(4000));
+            jg.rotateX(millis() / (float)(80000));
+
+            jg.sphere(moonimplodesize);
+            jg.popMatrix();
+
 
             jg.fill(0, 0, 0);
-            jg.circle(jg.width / 2, (jg.height * (float)0.65), (jg.width * (float)0.26));
+            jg.circle(moonX, moonY, moonsize * 2);
             for(int i = 0; i < jg.getAudioBuffer().size(); i++) 
             {
                 jg.noFill();
@@ -118,21 +160,21 @@ public class JasonsVisual extends Visual {
                 //looping colours
                 int hue = (i%105);
                 jg.stroke(hue, 255, 255);
-                jg.circle(jg.width / 2, (jg.height * (float)0.65), (jg.getAudioBuffer().get(i) * multiplier) + (jg.width * (float)0.26));
+                jg.circle(moonX, moonY, (jg.getAudioBuffer().get(i) * multiplier) + (moonsize* 2));
             }
                 
             
         }
 
         //placeholder for implosion
-        else if (countdown < 1 && moonsize < 0)
+        else if (countdown < 1 && moonimplodesize < 0)
         {
             //fill(0, 0, 255);
             //jg.text("Moon Detonation Immenent\n0.00", jg.width / 2, jg.height * (float)0.2);
 
             
             jg.fill(0, 0, 0);
-            jg.circle(jg.width / 2, (jg.height * (float)0.65), (jg.width * (float)0.26));
+            jg.circle(moonX, moonY, moonsize * 2);
             for(int i = 0; i < jg.getAudioBuffer().size(); i++) 
             {
                 jg.noFill();
@@ -140,7 +182,7 @@ public class JasonsVisual extends Visual {
                 //looping colours
                 int hue = (i%105);
                 jg.stroke(hue, 255, 255);
-                jg.circle(jg.width / 2, (jg.height * (float)0.65), (jg.getAudioBuffer().get(i) * multiplier) + (jg.width * (float)0.26));
+                jg.circle(moonX, moonY, (jg.getAudioBuffer().get(i) * multiplier) + (moonsize* 2));
                     
                 //jg.stroke(hue, 255, 255);
                 //float lineX = map(i, 0, jg.getAudioBuffer().size(), 0, jg.width);
@@ -148,7 +190,6 @@ public class JasonsVisual extends Visual {
             }
         }
     }
-
 
 }
 
