4587e15803d93db8fbd169aae2beb1a72a60ebf0
c22516126 <124163643+c22516126@users.noreply.github.com>
2024-03-24 04:29:05 +0000

    moon implosion

--- java/src/C22516126/JasonsVisual.java
+++ java/src/C22516126/JasonsVisual.java
@@ -62,6 +62,7 @@ public class JasonsVisual extends Visual {
             jg.circle(0, 0, (jg.getAudioBuffer().get(i) * multiplier) + (jg.width * 2));
             jg.popMatrix();
 
+
         }
         
         //before moon detonates
@@ -106,10 +107,60 @@ public class JasonsVisual extends Visual {
 
             jg.sphere(moonsize);
             jg.popMatrix();
+
+
+            jg.fill(0, 0, 0);
+            jg.circle(jg.width / 2, (jg.height * (float)0.65), (jg.width * (float)0.26));
+            for(int i = 0; i < jg.getAudioBuffer().size(); i++) 
+            {
+                jg.noFill();
+
+                //looping colours
+                int hue = (i%105);
+                jg.stroke(hue, 255, 255);
+                jg.circle(jg.width / 2, (jg.height * (float)0.65), (jg.getAudioBuffer().get(i) * multiplier) + (jg.width * (float)0.26));
+            }
                 
             
+        }
+
+        //placeholder for implosion
+        else if (countdown < 1 && moonsize < 0)
+        {
+            //fill(0, 0, 255);
+            //jg.text("Moon Detonation Immenent\n0.00", jg.width / 2, jg.height * (float)0.2);
+
+            
+            jg.fill(0, 0, 0);
+            jg.circle(jg.width / 2, (jg.height * (float)0.65), (jg.width * (float)0.26));
+            for(int i = 0; i < jg.getAudioBuffer().size(); i++) 
+            {
+                jg.noFill();
+
+                //looping colours
+                int hue = (i%105);
+                jg.stroke(hue, 255, 255);
+                jg.circle(jg.width / 2, (jg.height * (float)0.65), (jg.getAudioBuffer().get(i) * multiplier) + (jg.width * (float)0.26));
+                    
+                //jg.stroke(hue, 255, 255);
+                //float lineX = map(i, 0, jg.getAudioBuffer().size(), 0, jg.width);
+                //jg.line(lineX, (jg.getAudioBuffer().get(i) * (jg.width / 6)) + jg.height/2, lineX, (jg.getAudioBuffer().get(i) * (jg.height / 6)) + jg.height/2);
+            }
         }
     }
 
 
 }
+
+/*
+ 
+            for(int i = 0 ; i < ab.size() ; i ++)
+            {
+                sum += abs(ab.get(i));
+                lerpedBuffer[i] = lerp(lerpedBuffer[i], ab.get(i), 0.05f);
+
+                float hue = map(i, 0, ab.size(), 200, 255);
+                stroke(hue, 255, 255);
+                line(300 + i, (ab.get(i) * (cx) / 2)+400, 300 + i, (ab.get(i) * (cy)/2 )+400); 
+ 
+ */
